7af67ce026a46398d0832687b95fba8053870cf0
Nicholas Modesto <nicholas.modesto@carrierenterprise.com>
2024-06-18 23:25:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const response = await fetch(url);
                 console.log("dev: fetch success")
             const data = await response.json();
-                console.log ("dev: " + data);
+                console.log ("dev: " + JSON.stringify(data));
             const movies = data.feed.entry.map(entry => ({
                 title: entry.gsx$title.$t,
                 format: entry.gsx$format.$t,

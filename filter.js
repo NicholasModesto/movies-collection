24ff70694ad02a7b3c3e6bc085c435a96d4f4cfe
Nicholas Modesto <nicholas.modesto@carrierenterprise.com>
2025-05-31 22:33:05 -0500

    Debounce & Cleanup

--- filter.js
+++ filter.js
@@ -1,20 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-const table = document.querySelector("table");
+    const table = document.querySelector("table");
+    const searchInput = document.querySelector("input");
+    let debounceTimer;
       
-function filter(){
-  const query = document.querySelector("input").value.toLowerCase();
-  const rows = table.rows;  
-
-//  if(query.length < 3) return;  // optional parameter
-  
-  for (let z=1 ; z<rows.length ; z++)
-    {
-	  if(rows[z].cells[0].innerText.toLowerCase().includes(query) ) rows[z].style.display="table-row";
-      else rows[z].style.display="none";
+    function filter(){
+        const query = searchInput.value.toLowerCase();
+        const rows = table.rows;  
+        
+        for (let z=1; z<rows.length; z++) {
+            if(rows[z].cells[0].innerText.toLowerCase().includes(query)) {
+                rows[z].style.display="table-row";
+            } else {
+                rows[z].style.display="none";
+            }
+        }
     }
-}
-
-document.querySelector("input").addEventListener("input", filter);
-
+    
+    function debouncedFilter() {
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(filter, 300);
+    }
+    
+    searchInput.addEventListener("input", debouncedFilter);
 });
--- script.js
+++ script.js
@@ -15,27 +15,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function sortMoviesByTitle(movies) {
         return movies.sort((a, b) => {
-            const titleA = a.title.replace(/^The\s+/i, '');
-            const titleB = b.title.replace(/^The\s+/i, '');
+            const titleA = a.title.replace(/^The\\s+/i, '');
+            const titleB = b.title.replace(/^The\\s+/i, '');
             return titleA.localeCompare(titleB);
         });
     }
 
     async function fetchMovies() {
         const apiKey = 'AIzaSyD0NTvju2gQOz-RlnmQdoR00cSvP-iRnw4';
-
-// TODO update sheetID
-// TODO consolidate music stuff
-
         const sheetId = '12ahIyxGW0R32JIzTV99xi_zVkgX4e4uMF9xdLw3b0ZQ';
         const range = 'Sheet1!A2:C1000';
         const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`;
 
         try {
             const response = await fetch(url);
-                //console.log("dev: fetch success")
             const data = await response.json();
-                //console.log ("dev: " + JSON.stringify(data));
             
             const moviesCollection = { movies: [] };
             
